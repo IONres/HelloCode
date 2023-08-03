6523f6ae9a484fa035e6654ccc7f59b199307039
Ольга Ильина <MoyaPochtaOI@mail.ru>
2023-08-03 23:39:09 +0300

    add random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
 ﻿// сложение двух чисел
-int numberA = 3;
-int numberB = 5;
+int numberA = new Random().Next(1,10); //1,2,3...9
+Console.WriteLine (numberA); //выводит случайное число А
+int numberB = new Random(). Next(1,10);
+Console.WriteLine (numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
